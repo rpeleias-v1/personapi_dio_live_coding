96fb928aa9752549562d2a7729afec1691beca70
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-29 23:41:33 -0300

    Created base classes for tests

---
+++ src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,4 @@
+package one.digitalinnovation.personapi.service;
+
+public class PersonServiceTest {
+}
---
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.personapi.utils;
+
+import java.time.LocalDate;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Rodrigo";
+    private static final String LAST_NAME = "Peleias";
+    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+//    public static PersonDTO createFakeDTO() {
+//        return PersonDTO.builder()
+//                .firstName(FIRST_NAME)
+//                .lastName(LAST_NAME)
+//                .cpf(CPF_NUMBER)
+//                .birthDate("04-04-2010")
+//                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+//                .build();
+//    }
+//
+//    public static Person createFakeEntity() {
+//        return Person.builder()
+//                .id(PERSON_ID)
+//                .firstName(FIRST_NAME)
+//                .lastName(LAST_NAME)
+//                .cpf(CPF_NUMBER)
+//                .birthDate(BIRTH_DATE)
+//                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+//                .build();
+//    }
+}
