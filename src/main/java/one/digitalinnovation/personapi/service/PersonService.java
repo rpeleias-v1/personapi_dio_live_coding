e03bdb42b29b530cbd8da0650c90323d6d9ecf1c
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-30 08:33:01 -0300

    Rfactored findById oepration

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -42,10 +42,17 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Optional<Person> optionalPerson = personRepository.findById(id);
-        if (optionalPerson.isEmpty()) {
-            throw new PersonNotFoundException(id);
-        }
-        return personMapper.toDTO(optionalPerson.get());
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+
+        return personMapper.toDTO(person);
     }
+
+    public MessageResponseDTO update(Long id, PersonDTO personDTO) {
+        return null;
+    }
+
+
+
+
 }
