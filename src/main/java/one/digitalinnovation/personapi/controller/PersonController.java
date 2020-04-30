66214b90be469cde07a6c8b713de59aaaad2a13b
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-29 22:28:29 -0300

    Created first commit

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API Test!";
+    }
+}
